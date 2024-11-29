8a4bf3c1299374db3053164643961a331a2b46c5
Dominik <dominikja@o2.pl>
2024-11-29 21:43:11 +0100

    Changes made lala

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
             <li>
               Get started by editing <code>pages/index.js</code>.
             </li>
-            <li>Save and see your changes instantly.</li>
+            <li>Save and see your changes instantly now.</li>
           </ol>
 
           <div className={styles.ctas}>
